f9f561c161f6148abe85ac6d3bfd67943b0d184a
susun <2734458153@qq.com>
2020-12-05 00:09:36 +0800

    提交代码

---
+++ config/local_setting.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/food_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = "utf8mb4"
